9dcf57e004b7333595763d3c72529c8a084e2f1d
M.Ravi <mondi.ravi@gmail.com>
2017-07-13 13:32:43 +0200

    Added some basic functionality to consumers.py

--- hosting/consumers.py
+++ hosting/consumers.py
@@ -1,19 +1,40 @@
 from channels import Group
 from channels.auth import channel_session_user, channel_session_user_from_http
+from channels.security.websockets import allowed_hosts_only
 import json
+import logging
+
+log = logging.getLogger(__name__)
 
 
+@allowed_hosts_only
 @channel_session_user_from_http
-def ws_connect(message):
-    channelid = 'users_{}'.format(message.user.id)
-    print('Subscribing to {}'.format(channelid))
-    Group(channelid).add(message.reply_channel)
-    Group(channelid).send({
-        'text': json.dumps({
-            'username': message.user.id,
-            'is_logged_in': True
-        })
-    })
+def ws_connect_hosting(message, channel_name):
+    # Accept the incoming connection
+    message.reply_channel.send({"accept": True})
+    channel_id = 'users_{}'.format(channel_name)
+    print('Subscribing to {}'.format(channel_id))
+    Group(channel_id).add(message.reply_channel)
+
+
+@allowed_hosts_only
+@channel_session_user_from_http
+def ws_receive_hosting(message, channel_name):
+    channel_id = 'users_{}'.format(channel_name)
+    try:
+        data = json.loads(message)
+    except ValueError:
+        log.debug("ws message isn't json text=%s", message)
+        return
+
+    if data:
+        if data['action'] == "delete_vm":
+            # Tell client task has been started
+            Group(channel_id).send({
+                "text": json.dumps({
+                    "action": "started",
+                })
+            })
 
 
 @channel_session_user
