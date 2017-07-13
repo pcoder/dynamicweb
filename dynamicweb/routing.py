7b9f8cd095792f781b11eed660138c7eab636b99
M.Ravi <mondi.ravi@gmail.com>
2017-07-13 13:32:06 +0200

    Updated routing url to identify hosting app

--- dynamicweb/routing.py
+++ dynamicweb/routing.py
@@ -1,8 +1,8 @@
 from channels.routing import route
-from hosting.consumers import ws_connect, ws_disconnect
+from hosting.consumers import ws_connect_hosting, ws_disconnect
 
 
 channel_routing = [
-    route('websocket.connect', ws_connect),
+    route('websocket.connect', ws_connect_hosting, path='^/hosting/(?P<channel_name>[a-zA-Z0-9_]+)/$'),
     route('websocket.disconnect', ws_disconnect),
 ]
