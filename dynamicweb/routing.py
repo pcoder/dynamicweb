b02de19ee55aee1c0a4e0c6bbfabdb6a47f6b499
M.Ravi <mondi.ravi@gmail.com>
2017-07-17 09:14:54 +0200

    Added routing for websocket endpoints

--- dynamicweb/routing.py
+++ dynamicweb/routing.py
@@ -1,8 +1,8 @@
 from channels.routing import route
-from hosting.consumers import ws_connect_hosting, ws_disconnect
-
+from hosting.consumers import ws_connect_hosting, ws_receive_hosting, ws_disconnect_hosting
 
 channel_routing = [
-    route('websocket.connect', ws_connect_hosting, path='^/hosting/(?P<channel_name>[a-zA-Z0-9_]+)/$'),
-    route('websocket.disconnect', ws_disconnect),
+    route('websocket.connect', ws_connect_hosting, path='^/hosting/(?P<vm_id>[^/]+)/$'),
+    route("websocket.receive", ws_receive_hosting, path='^/hosting/(?P<vm_id>[^/]+)/$'),
+    route('websocket.disconnect', ws_disconnect_hosting, path='^/hosting/(?P<vm_id>[^/]+)/$'),
 ]
