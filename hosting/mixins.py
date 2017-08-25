52474106d9d9c00426376b634f049c214c749f94
PCoder <purple.coder@yahoo.co.uk>
2017-08-25 14:54:23 +0530

    ProcessVMSelectionMixin: Handled possible TypeError

--- hosting/mixins.py
+++ hosting/mixins.py
@@ -5,22 +5,30 @@ from opennebula_api.serializers import VirtualMachineTemplateSerializer
 from opennebula_api.models import OpenNebulaManager
 
 from .models import HostingPlan
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class ProcessVMSelectionMixin(object):
-
     def post(self, request, *args, **kwargs):
+        type_error = False
+        try:
+            template_id = int(request.POST.get('vm_template_id'))
+            configuration_id = int(request.POST.get('configuration'))
+            template = OpenNebulaManager().get_template(template_id)
+            data = VirtualMachineTemplateSerializer(template).data
+            configuration = HostingPlan.objects.get(id=configuration_id)
 
-        template_id = int(request.POST.get('vm_template_id'))
-        configuration_id = int(request.POST.get('configuration'))
-        template = OpenNebulaManager().get_template(template_id)
-        data = VirtualMachineTemplateSerializer(template).data
-        configuration = HostingPlan.objects.get(id=configuration_id)
+            request.session['template'] = data
+            request.session['specs'] = configuration.serialize()
+        except TypeError as te:
+            logger.debug(
+                "Type error in ProcessVMSelectionMixin. Details: {}".format(
+                    str(te)))
+            type_error = True
 
-        request.session['template'] = data
-        request.session['specs'] = configuration.serialize()
-
-        if not request.user.is_authenticated():
+        if not request.user.is_authenticated() or type_error:
             request.session['next'] = reverse('hosting:payment')
             return redirect(reverse('hosting:login'))
         return redirect(reverse('hosting:payment'))
