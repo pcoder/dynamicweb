9c37f84988b7b6a24d937bb325e886332b5e396e
PCoder <purple.coder@yahoo.co.uk>
2018-08-29 08:15:23 +0200

    Enable debug_toolbar

--- dynamicweb/settings/local.py
+++ dynamicweb/settings/local.py
@@ -20,5 +20,13 @@ MIDDLEWARE_CLASSES += ("debug_toolbar.middleware.DebugToolbarMiddleware",)
 INSTALLED_APPS += (
     'django_extensions',
     # debug_toolbar seems to conflict with multisite (and djangocms_multisite)
-    #   'debug_toolbar'
+    'debug_toolbar',
     )
+
+INTERNAL_IPS=('127.0.0.1',)
+
+def show_toolbar(request):
+    return True
+DEBUG_TOOLBAR_CONFIG = {
+    "SHOW_TOOLBAR_CALLBACK" : show_toolbar,
+}
